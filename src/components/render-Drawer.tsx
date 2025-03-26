80595dbd952fec11284b0e8e0a2838a9b31fde18
marlon <marlonsuyuc@gmail.com>
2025-03-26 13:36:11 -0600

    refactor: update Header and Drawer components for improved structure and accessibility

--- src/components/render-Drawer.tsx
+++ src/components/render-Drawer.tsx
@@ -1,16 +1,57 @@
 import { Button } from '@/components/ui/button'
-import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
-import { Menu } from 'lucide-react'
+import {
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle
+} from '@/components/ui/drawer'
+import clsx from 'clsx'
+import { FolderCode, Mail, Menu, User } from 'lucide-react'
+import { useState } from 'react'
 
-export function DrawerDemo({ children }: { children: React.JSX.Element }) {
+const pages = [
+	{ label: 'Proyects', href: '#proyects', icon: FolderCode },
+	{ label: 'About', href: '#about', icon: User },
+	{ label: 'Contact', href: '#contact', icon: Mail }
+]
+
+export function DrawerDemo() {
+	const [open, setOpen] = useState(false)
+
 	return (
-		<Drawer>
-			<DrawerTrigger asChild>
-				<Button variant="ghost" size="icon" className="size-12 text-primary">
-					<Menu className="size-8" />
+		<>
+			<Drawer open={open} onOpenChange={setOpen} fixed={false}>
+				<Button variant="ghost" size="icon" onClick={() => setOpen(true)}>
+					<Menu className="size-8 text-white/80" />
+					<span className="sr-only">Open menu</span>
 				</Button>
-			</DrawerTrigger>
-			<DrawerContent>{children}</DrawerContent>
-		</Drawer>
+
+				<DrawerContent className="text-white">
+					<DrawerHeader>
+						<DrawerTitle></DrawerTitle>
+						<DrawerDescription></DrawerDescription>
+					</DrawerHeader>
+
+					{pages.map(({ label, href, icon: Icon }) => {
+						// console.log(icon)
+						return (
+							<a
+								key={href}
+								href={href}
+								onClick={() => setOpen(false)}
+								className={clsx([
+									'flex cursor-pointer items-center justify-center gap-[6px] border-b border-neutral-900/50 py-4 font-medium hover:text-muted-foreground'
+								])}
+							>
+								<Icon className="size-[18px]" />
+
+								{label}
+							</a>
+						)
+					})}
+				</DrawerContent>
+			</Drawer>
+		</>
 	)
 }
