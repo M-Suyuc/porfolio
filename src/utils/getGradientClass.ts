4cd54012c9278932c8162ca2f6307b279bf6a701
marlon <marlonsuyuc@gmail.com>
2025-04-16 16:29:49 -0600

    style: update getGradientClasses to use new gradient syntax and shadows

--- src/utils/getGradientClass.ts
+++ src/utils/getGradientClass.ts
@@ -1,13 +1,13 @@
 export const getGradientClasses = (index: number) => {
 	const gradients = [
-		'bg-[linear-gradient(120deg,var(--tw-gradient-stops))] from-yellow-300/80 to-rose-600/80 shadow-rose-600/20',
-		'bg-[linear-gradient(40deg,var(--tw-gradient-stops))] from-pink-300/80 to-violet-500/40 shadow-violet-500/20',
-		'bg-[linear-gradient(140deg,var(--tw-gradient-stops))] from-cyan-400/80 to-indigo-700/50 shadow-indigo-700/20',
-		'bg-[linear-gradient(140deg,var(--tw-gradient-stops))] from-orange-500/50 to-yellow-500/80 shadow-yellow-500/20',
-		'bg-[linear-gradient(120deg,var(--tw-gradient-stops))] from-green-400/70 to-cyan-600/80 shadow-cyan-600/20',
-		'bg-[linear-gradient(120deg,var(--tw-gradient-stops))] from-green-400/70 to-cyan-600/80 shadow-cyan-600/20',
-		'bg-[linear-gradient(180deg,var(--tw-gradient-stops))] from-gray-200/40 to-rose-500/80 shadow-rose-500/20',
-		'bg-[linear-gradient(40deg,var(--tw-gradient-stops))] from-pink-300/80 to-violet-500/40 shadow-violet-500/20'
+		'bg-gradient-to-r from-yellow-300/80 to-rose-600/80 shadow-[0_4px_6px_-1px_rgba(244,63,94,0.2)]',
+		'bg-gradient-to-r from-pink-300/80 to-violet-500/40 shadow-[0_4px_6px_-1px_rgba(139,92,246,0.2)]',
+		'bg-gradient-to-r from-cyan-400/80 to-indigo-700/50 shadow-[0_4px_6px_-1px_rgba(79,70,229,0.2)]',
+		'bg-gradient-to-r from-orange-500/50 to-yellow-500/80 shadow-[0_4px_6px_-1px_rgba(234,179,8,0.2)]',
+		'bg-gradient-to-r from-green-400/70 to-cyan-600/80 shadow-[0_4px_6px_-1px_rgba(6,182,212,0.2)]',
+		'bg-gradient-to-r from-green-400/70 to-cyan-600/80 shadow-[0_4px_6px_-1px_rgba(6,182,212,0.2)]',
+		'bg-gradient-to-r from-gray-200/40 to-rose-500/80 shadow-[0_4px_6px_-1px_rgba(244,63,94,0.2)]',
+		'bg-gradient-to-r from-pink-300/80 to-violet-500/40 shadow-[0_4px_6px_-1px_rgba(139,92,246,0.2)]'
 	]
 	return gradients[index] || ''
 }
